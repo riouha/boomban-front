dc42486764247210ab3857ef47fa3b0adcc769f1
riouha <4riouha@gmail.com>
2022-12-20 13:46:40 +0330

    .

---
+++ zest.js
@@ -0,0 +1,9 @@
+function chunkArray(array, chunkSize) {
+  const result = [];
+  for (let i = 0; i < array.length; i += chunkSize) {
+    result.push(array.slice(i, i + chunkSize));
+  }
+  return result;
+}
+
+console.log(chunkArray([12, 22, 33], 4));
