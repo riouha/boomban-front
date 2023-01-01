3c800ae3f57ca56998ac3272bbe8d98e7a7381c7
riouha <4riouha@gmail.com>
2023-01-01 20:30:18 +0330

    .

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -16,11 +16,16 @@ export default function Home() {
           <div className='hot_post'>
             <LargeOverlayedPost
               post={{
+                id: 1,
+                slug: '1',
                 title: 'جدیدترین پیش بینی قیمت مسکن در سال جدید',
                 image: 'https://katen.ny3.ir/rtl/images/posts/featured-lg.jpg',
                 category: 'الهام بخش',
                 date: '27/9/1401',
-                author: 'نام نویسنده',
+                author: {
+                  id: 1,
+                  title: 'نام نویسنده',
+                },
               }}
             />
           </div>
