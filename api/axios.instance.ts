76b6214f8f7d30f442ea8baf6100e378eebe370b
riouha <4riouha@gmail.com>
2023-01-01 15:16:02 +0330

    change api url

--- api/axios.instance.ts
+++ api/axios.instance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 console.log(process.env.NODE_ENV);
 
-export const BaseURL = process.env.NODE_ENV === 'production' ? 'http://87.248.156.231:3400' : 'http://localhost:3400';
+export const BaseURL = process.env.NODE_ENV === 'production' ? 'http://87.107.146.188:3400' : 'http://localhost:3400';
 export const axiosInstance = axios.create({
   baseURL: BaseURL,
   headers: {},
